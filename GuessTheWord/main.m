a91ef8f0f9603c64caaf21bf6e221586018ed888
mitrenegade <bobbyren@gmail.com>
2015-03-14 12:55:37 -0400

    Initial Commit

---
+++ GuessTheWord/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GuessTheWord
+//
+//  Created by Bobby Ren on 3/14/15.
+//  Copyright (c) 2015 Bobby Ren Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
